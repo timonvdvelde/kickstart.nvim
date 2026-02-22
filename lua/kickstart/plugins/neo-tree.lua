02be33bf93b253244576e6ea782b0d677d660175
Timon van de Velde <19689305+timonvdvelde@users.noreply.github.com>
2026-02-22 11:47:59 +0100

    Add reveal to neo tree so it shows the highlighted file

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree float<CR>', desc = 'NeoTree reveal', silent = true },
+    { '\\', ':Neotree float reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
