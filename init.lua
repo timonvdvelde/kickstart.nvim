5ecb8f237c6a19eb2e9d4044a45a437f2d740e6f
Timon van de Velde <19689305+timonvdvelde@users.noreply.github.com>
2026-02-22 11:50:07 +0100

    Set prettierd as formatting default for some languages

--- init.lua
+++ init.lua
@@ -713,7 +713,13 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd' },
+        typescript = { 'prettierd' },
+        css = { 'prettierd' },
+        html = { 'prettierd' },
+        json = { 'prettierd' },
+        yaml = { 'prettierd' },
+        markdown = { 'prettierd' },
       },
     },
   },
